5dccc1b11d442101538310449595157e1bda01e8
subh <subhranshu497@gmail.com>
2024-12-04 23:51:29 -0700

    commit 12052024

---
+++ src/com/prep/MovePiecesToObtainAString.java
@@ -0,0 +1,27 @@
+package com.prep;
+
+public class MovePiecesToObtainAString {
+    public static void main(String[] args) {
+        String source = "_L__R__R_";
+        String target = "L______RR";
+        boolean flag = canChange(source, target);
+        System.out.println(flag);
+    }
+
+    private static boolean canChange(String source, String target) {
+        int n = source.length();
+        int i = 0;
+        int j = 0;
+        while (i <n || j<n){
+            while(i<n && source.charAt(i)=='_')i++;
+            while(j<n && target.charAt(j)=='_')j++;
+            if(i==n || j==n) return i==n && j==n;
+            if(source.charAt(i) != target.charAt(j)) return false;
+            if(source.charAt(i)=='R' && i>j) return false;
+            if(source.charAt(i)=='L' && i<j) return false;
+            i++;
+            j++;
+        }
+        return true;
+    }
+}
